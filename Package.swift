9f75cb713b78639995af04f7fd5792d3264f1bb9
vamsii777 <vamsi@dewonderstruck.com>
2024-11-17 13:17:40 +0530

    feat: migrate test suite to Swift Testing

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         .watchOS(.v8),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "JuspayKit",
             targets: ["JuspayKit"]
@@ -24,24 +23,25 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "JuspayKit", dependencies: [
-            .product(name: "AsyncHTTPClient", package: "async-http-client"),
-            .product(name: "Crypto", package: "swift-crypto"),
-        ],
-        swiftSettings: swiftSettings),
+        .target(
+            name: "JuspayKit",
+            dependencies: [
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "Crypto", package: "swift-crypto"),
+            ],
+            swiftSettings: swiftSettings
+        ),
         .testTarget(
             name: "JuspayKitTests",
             dependencies: ["JuspayKit"]
-        ),
+        )
     ]
 )
 
-var swiftSettings: [SwiftSetting] {
+var swiftSettings: [SwiftSetting] { 
     [
         .enableUpcomingFeature("StrictConcurrency"),
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("FullTypedThrows"),
-    ]
+    ] 
 }
